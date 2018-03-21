bd705e7a49fb1a56cecca2fd0c9afc96f47366ba
formacion <formacion@192.168.26.130>
2018-03-21 11:01:39 +0100

    o

--- src/test/java/es/fpdual/eadmin/eadmin/modelo/DocumentoRegistroTests.java
+++ src/test/java/es/fpdual/eadmin/eadmin/modelo/DocumentoRegistroTests.java
@@ -5,12 +5,12 @@ import java.util.Date;
 public class DocumentoRegistroTests {
 	
 	private static final Integer codigo_Registro = 1;
-	private static final Date fecha_Creacion= new Date();
+	private static final Date fecha_Creacion1= new Date();
 	private static final String nombre_String = "nombre";
 	private static final Boolean documento_Publico = true;
 	private static String nifInteresado = "12345678A";
 	private static String codigoRegistro;
 	
-	DocumentoRegistro documentoRegistro1 = new DocumentoRegistro(codigo_Registro,nombre_String,fecha_Creacion,
+	DocumentoRegistro documentoRegistro1 = new DocumentoRegistro(codigo_Registro,nombre_String,fecha_Creacion1,
 			documento_Publico, EstadoDocumento.ACTIVO,nifInteresado,codigoRegistro);
 }
