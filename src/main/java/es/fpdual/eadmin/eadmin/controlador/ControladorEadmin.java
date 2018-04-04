68a5caec8a39cb8c1f5d76e219c43a9628b32d9e
ezxioj87 <juandediosbermudo@hotmail.com>
2018-04-04 09:46:25 +0200

    commit 04/04/2018 9:46

---
+++ src/main/java/es/fpdual/eadmin/eadmin/controlador/ControladorEadmin.java
@@ -0,0 +1,50 @@
+package es.fpdual.eadmin.eadmin.controlador;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.fpdual.eadmin.eadmin.modelo.Documento;
+import es.fpdual.eadmin.eadmin.servicios.ServicioDocumento;
+
+@RestController
+public class ControladorEadmin {
+	
+	private final ServicioDocumento servicioDocumento;
+	
+	@Autowired
+	public ControladorEadmin(ServicioDocumento servicioDocumento) {
+		this.servicioDocumento = servicioDocumento;
+	}
+	
+	@GetMapping(value="/eadmin/documentos")
+	public ResponseEntity<List<Documento>> getTodosDocumentos(){
+		return new ResponseEntity<List<Documento>>(
+				servicioDocumento.obtenerTodosLosDocumentos(), HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/eadmin/documentos/{codigo}")
+	public ResponseEntity<Documento> getDocumentoPorCodigo(@PathVariable("codigo") Integer codigo){
+		if(Objects.nonNull(codigo)) {
+		return new ResponseEntity<Documento>(
+				servicioDocumento.obtenerDocumentoPorCodigo(codigo), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	@DeleteMapping(value="/eadmin/documentos/eliminar/{codigo}")
+	public ResponseEntity<?> eliminarDocumeto(@PathVariable("codigo") Integer codigo){
+		this.servicioDocumento.eliminarDocumento(codigo);
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+		
+	}
+
+}
